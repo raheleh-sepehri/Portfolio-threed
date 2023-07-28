c56a4afa5cf543de028676502b00dfdf379d9554
Raheleh sepehri <raheleh1392@gmail.com>
2023-07-28 12:44:01 +0330

    build aa navbar (left and right side(

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { styles } from "../styles";
+import { Link } from "react-router-dom";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <div
+        className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      >
+        <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+          <Link
+            className="flex items-center "
+            to="./"
+            onClick={() => {
+              setActive("");
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img
+              src="./logo.png"
+              className="w-10  rounded-full h-10 object-fill"
+            />
+            <p className="font-bold text-[18px] cursor-pointer text-white ml-1 ">
+              {" "}
+              Raheleh{" "}
+              <span className="sm:block hidden">| Frontend Developer</span>
+            </p>
+          </Link>
 
-export default Navbar
+          <ul className="sm:flex hidden gap-10 list-none  ">
+            {navLinks.map((link) => (
+              <li key={link.id}>
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
