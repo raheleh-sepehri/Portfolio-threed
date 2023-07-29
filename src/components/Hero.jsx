25477ea4e976603fb92ae1c3b7127a6c83b43cab
Raheleh sepehri <raheleh1392@gmail.com>
2023-07-29 21:19:23 +0330

    add div animate to go on about

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,6 +24,22 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+
+      <div className="absolute xs:-bottom-2 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="flex justify-center p-2 items-start w-[35px] h-[64px] rounded-3xl border-4 border-secondary   ">
+            <motion.div
+              transition={{
+                duration: 1.5,
+                repeat:Infinity,
+                repeatType: "loop",
+              }}
+              animate={{ y: [0, 32, 0] }}
+              className="w-3 h-3 mb-1 rounded-full bg-secondary"
+            ></motion.div>
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
