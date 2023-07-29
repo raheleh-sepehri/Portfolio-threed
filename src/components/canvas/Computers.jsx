eddd829212e73e1b885221aa9ed3430c8b6bc768
Raheleh sepehri <raheleh1392@gmail.com>
2023-07-29 21:43:36 +0330

    set mobile size and change some codes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,9 @@
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   console.log(computer);
 
@@ -13,7 +13,7 @@ const Computers = () => {
       <pointLight intensity={4} />
       <hemisphereLight intensity={1} />
       <spotLight
-        position={[-20, 50, 10]}
+        position={isMobile ? [0, -3, -2.2] : [-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
@@ -22,7 +22,7 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
+        scale={isMobile ? 0.4 : 0.75}
         position={[0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -31,6 +31,27 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    //add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("max-width:500px");
+
+    //set the initial value of the isMobile state variable
+    setIsMobile(mediaQuery.matches);
+
+    //define a callback func to handle changes the media 
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    //add the callback func to as a listener for changes to the media 
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    //removed the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
   return (
     <Canvas
       frameloop="demand"
@@ -46,7 +67,7 @@ const ComputersCanvas = () => {
         />
         {/* The ambient light can be kept here */}
 
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
