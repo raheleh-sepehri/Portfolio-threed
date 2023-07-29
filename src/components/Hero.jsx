cc84c4323b8794dbe3b2c02aff23a57652972cf4
Raheleh sepehri <raheleh1392@gmail.com>
2023-07-29 21:43:00 +0330

    change bottom position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
 
-      <div className="absolute xs:-bottom-2 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:-bottom-2 -bottom-7 w-full flex justify-center items-center">
         <a href="#about">
           <div className="flex justify-center p-2 items-start w-[35px] h-[64px] rounded-3xl border-4 border-secondary   ">
             <motion.div
