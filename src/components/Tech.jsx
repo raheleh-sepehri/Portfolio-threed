aac3e020ee5822d74d429c7c520df9eca27c79db
Raheleh sepehri <raheleh1392@gmail.com>
2023-08-02 17:04:42 +0330

    Develope Tech Component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import {BallCanvas} from './canvas'
+import { SectionWrapper } from '../hoc'
+import { technologies } from '../constants'
+
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>{technologies.map((tec,index)=>(
+      <div className='w-228 h-28' key={tec.name}>
+        <BallCanvas icon={tec.icon}/>
+      </div>
+    ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech,"")
