df604e70fbd6fb6c700ab0712f481f6b5ca6be15
Raheleh sepehri <raheleh1392@gmail.com>
2023-08-05 16:53:12 +0330

    ChangeLoader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
-import { Html, useProgress } from "@react-three/drei";
+import { Html } from "@react-three/drei";
 
 const CanvasLoader = () => {
-  const { progress } = useProgress();
+
   return (
     <Html
       as="div"
@@ -14,16 +14,7 @@ const CanvasLoader = () => {
       }}
     >
       <span className="canvas-loader"></span>
-      <p
-        style={{
-          fontSize: 14,
-          color: "#F1F1F1",
-          fontWeight: 800,
-          marginTop: 40,
-        }}
-      >
-        {progress.toFixed(2)}%
-      </p>
+     
     </Html>
   );
 };
